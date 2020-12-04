55a01fdf56fe0cc62f28b1e02f1243fcdc653b7d
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2020-12-04 02:35:58 -0800

    3b done on first try!

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { day1a, day1b } = require("./solutions/day1");
 const { day2a, day2b } = require("./solutions/day2");
-const { day3a } = require("./solutions/day3");
+const { day3a, day3b, slopesFor3b } = require("./solutions/day3");
 
 const inputDay1 = require("./imports/day1");
 const inputDay2 = require("./imports/day2");
@@ -16,5 +16,6 @@ console.log(day2b(inputDay2));
 
 console.log("\n Day 3:");
 console.log(`Trees hit: ${day3a(inputDay3)}`);
+console.log(day3b(inputDay3, slopesFor3b));
 
 console.log("\n");
--- solutions/day3.js
+++ solutions/day3.js
@@ -18,6 +18,65 @@ function day3a(arrayOfStrings) {
   return treesHit;
 }
 
+function day3b(arrayOfStrings, arrayOfSlopes) {
+  let treesHitPerSlopeArray = [];
+  // each slope is a separate calculation
+  arrayOfSlopes.forEach((slopeArr) => {
+    const moveRight = slopeArr[0];
+    const moveDown = slopeArr[1];
+    let linesToSkip = 0;
+
+    let treesHit = 0;
+    let position = 0;
+    let stringLength = arrayOfStrings[0].length;
+
+    // for each line
+    arrayOfStrings.forEach((string) => {
+      // skip the line, if necessary
+      if (linesToSkip > 0) {
+        linesToSkip--;
+      } else {
+        // tally trees hit
+        if (string[position] === "#") {
+          treesHit++;
+        }
+
+        // move right the correct number of times
+        position += moveRight;
+
+        // reset if moved too far right
+        if (position >= stringLength) {
+          position -= stringLength;
+        }
+
+        // calculate how many lines to skip on next pass
+        linesToSkip = moveDown - 1;
+      }
+    });
+
+    treesHitPerSlopeArray.push(treesHit);
+  });
+
+  const answer = treesHitPerSlopeArray.reduce(function (
+    accumulator,
+    currentValue
+  ) {
+    return accumulator * currentValue;
+  });
+
+  return `Trees hit multiplied: ${answer}.`;
+}
+
+const slopesFor3b = [
+  [1, 1],
+  [3, 1],
+  [5, 1],
+  [7, 1],
+  [1, 2],
+];
+
 module.exports = {
   day3a,
+  day3b,
+  slopesFor3b,
 };
